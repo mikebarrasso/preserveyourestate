5fa46345d47db4b8adcacf0962fa352d778eafcb
mikebarrasso <mike@wealthline.co>
2026-09-04 16:27:36 -0400

    Add Google Analytics 4 tracking (G-S0DQ7Z2J61)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Source_Serif_4, Inter } from "next/font/google";
+import Script from "next/script";
 import Effects from "@/components/Effects";
 import "./globals.css";
 import siteConfig from "../data/site-config.json";
+
+const GA_MEASUREMENT_ID = "G-S0DQ7Z2J61";
 
 const display = Source_Serif_4({ 
   subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-display", display: "swap",
@@ -39,6 +42,18 @@ export default function RootLayout({
       <body>
         {children}
         <Effects />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
       </body>
     </html>
   );
